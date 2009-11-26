8b7cfd9da426c23fcba03979baa73dce6086dbe6
Sameer Ahuja <sameer.ahuja@gmail.com>
2009-11-26 10:22:06 +0000

    Transient state, LinkReaper not working for Test2

--- js/linkreaper.js
+++ js/linkreaper.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function(){
 	jQuery.noConflict();
-	LinkReaper.reapLinks('WINDO')
+	LinkReaper.reapLinks('WINDO');
+	//LinkReaper.reapAllLinks();
 });
 
 var LinkReaper = {
+	
+	var searchTerm = "";
+	var selectedLinks = [];
+	
 	reapAllLinks: function() {
 		jQuery('a').each(function() {
 			this.html("<span name='_Reaped' class='GleeReaped'>"+this.html()+"</span>");
@@ -11,12 +16,29 @@ var LinkReaper = {
 	},
 	
 	reapLinks: function(term) {
-		jQuery('a').each(function() {
-			var el = jQuery(this);
-			var index = el.html().toLowerCase().indexOf(term.toLowerCase());
-			if(index != -1) {
-				el.html("<span name='_Reaped' class='GleeReaped'>"+el.html()+"</span>");
-			}
-		});
-	}	
+		// If this term is a specialization of the last term
+		if(searchTerm.indexOf(term) == 0)
+		{
+			jQuery(selectedLinks).each(function(){
+				reapALink(jQuery(this), term);
+			});
+		}
+		// Else search the whole page
+		else
+		{
+			jQuery('a').each(function(){
+				reapALink(jQuery(this), term);
+			});
+		}
+	},
+	
+	reapALink: function(el, term) {
+		//var el = jQuery(this);
+		var index = el.html().toLowerCase().indexOf(term.toLowerCase());
+		if(index != -1) {
+			el.html("<span name='_Reaped' class='GleeReaped'>" \
+			+ el.html() 
+			+ "</span>");
+		}
+	}
 }
