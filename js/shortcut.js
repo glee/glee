d9422d3636a49dd880fec7ba1416a3f9f34178bb
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-11-26 08:38:45 +0000

    added jquery

--- js/shortcut.js
+++ js/shortcut.js
@@ -1,21 +1,37 @@
 window.addEvent('domready',function(){
+	
+	/* initialize the searchBox */
+	Glee.initBox();
+	
 	$(document).addEvent('keydown',function(e){
-		if(e.key == 'g' && e.control)
+		//Press control+g to activate searchBox
+		// if(e.key == 'g' && e.control)
+		if(e.key == 'g')
 		{
-			Glee.displayBox();
+			if(Glee.searchBox.getStyle('display') == "none")
+			{
+				Glee.searchBox.setStyle('display','block');
+			}
+			else
+			{
+				Glee.searchBox.setStyle('display','none');
+			}
 		}
 	});
 })
 
-var Glee = {
-	displayBox: function(){
+var Glee = { 
+	initBox: function(){
 		//creating the div to be displayed
 		var inputField = new Element('input', {
 			'class':'gleeSearchField'
 		});	
 		var searchBox = new Element('div', {
-			'id':'gleeBox'
+			'id':'gleeBox',
+			'html':'checking'
 		});
-		searchBox.adopt(inputField);
+		this.searchBox = searchBox;
+		// searchBox.adopt(inputField);	
+		$(document.body).adopt(searchBox);
 	}
 }
