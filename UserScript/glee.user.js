9bccee157bc56c5436dc0788ed51e411068a1f92
Sameer Ahuja <sameer.ahuja@gmail.com>
2009-11-27 00:53:29 +0000

    Added userscript. Working on firefox in my testing. To use this, install the latest version of greasemonkey and open this file in FireFox. You should see an install prompt.

---
+++ UserScript/glee.user.js
@@ -0,0 +1,271 @@
+// ==UserScript==
+// @name          Glee
+// @namespace     http://colloki.org/
+// @description   Keyboard Glee for your web
+// @include       *
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js
+// ==/UserScript==
+
+jQuery(document).ready(function(){
+	//activating the noConflict mode of jQuery
+	jQuery.noConflict();
+	
+	/* initialize the searchBox */
+	Glee.initBox();
+	
+	/* Setup CSS Styles */
+	var reaperCSS = '.GleeReaped{background-color: #fbee7e !important;border: 1px dotted #818181 !important;}';
+	
+	var gleeCSS = '#gleeBox{ position:fixed; left:5%; top:35%; display:none; overflow:auto; height:165px;width:90%; background-color:#333; opacity:0.65; color:#fff; margin:0;font-family:Calibri,"Times New Roman",Arial,serif; }#gleeSearchField{ width:90%; color:#fff; background-color:#333; margin:0; padding:5px;border:none; font-size:100px; font-family:Calibri,"Times New Roman",Arial,serif; }#gleeSubText{ padding:5px; color:#fff; float:left; }#gleeSubURL{ padding:5px; display:inline; float:right; }';
+	
+	GM_addStyle(reaperCSS + gleeCSS);
+		
+	// Bind Keys
+	jQuery(document).bind('keydown',function(e){
+		//pressing 'g' toggles the gleeBox
+		if(jQuery(e.target).attr('id') != 'gleeSearchField' && e.keyCode == 71)
+		{
+			e.preventDefault();
+			if(Glee.searchBox.css('display') == "none")
+			{
+				//reseting value of searchField
+				Glee.searchField.attr('value','');	
+				Glee.searchBox.fadeIn('fast');
+				Glee.searchField.focus();
+			}
+			else
+			{
+				Glee.searchBox.fadeOut('fast');
+			}
+		}
+	});
+	Glee.searchField.bind('keydown',function(e){
+		//pressing 'esc' hides the gleeBox
+		if(e.keyCode == 27)
+		{
+			e.preventDefault();			
+			LinkReaper.unreapAllLinks();			
+			//reseting value of searchField
+			Glee.searchField.attr('value','');				
+			Glee.searchBox.fadeOut('fast');		
+			Glee.searchField.blur();
+		}
+		else if(e.keyCode == 9)
+		{
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	});
+	Glee.searchField.bind('keyup',function(e){		
+		var value = Glee.searchField.attr('value');
+		if(e.keyCode == 9)
+		{
+			e.preventDefault();
+			if(Glee.searchField.attr('value') != "")
+			{
+				var el = LinkReaper.getNextLink();
+				Glee.setSubText(el);
+				Glee.scrollToLink(el);
+			}
+		}
+		else if(e.keyCode == 13 && Glee.subURL.text()!="")
+		{
+			e.preventDefault();
+			{
+				window.location = Glee.subURL.text();
+			}
+		}
+		else if(value.indexOf('.com') != -1)
+		{
+			Glee.setSubText(null);
+		}
+		else if(Glee.searchField.attr('value') != "")
+		{
+			// Reset value of searchField
+			LinkReaper.reapLinks(jQuery(this).attr('value'));
+			var el = LinkReaper.getNextLink();
+			Glee.setSubText(el);
+			Glee.scrollToLink(el);
+		} 
+		else if(Glee.searchField.attr('value') == "")
+		{
+			e.preventDefault();
+			LinkReaper.unreapAllLinks();
+			Glee.setSubText(null);
+		}
+	});
+});
+
+var Glee = { 
+	initBox: function(){
+		// Creating the div to be displayed
+		var searchField = jQuery("<input type=\"text\" id=\"gleeSearchField\" value=\"\" />");
+		var subText = jQuery("<div id=\"gleeSubText\">No Links selected</div>");
+		var subURL = jQuery("<div id=\"gleeSubURL\"></div>")
+		var searchBox = jQuery("<div id=\"gleeBox\"></div>");
+		var sub = jQuery("<div id=\"gleeSub\"></div>");
+		sub.append(subText);
+		sub.append(subURL);
+		searchBox.append(searchField);
+		searchBox.append(sub);
+		this.searchBox = searchBox;
+		this.searchField = searchField;
+		this.subText = subText;
+		this.subURL = subURL;
+		jQuery(document.body).append(searchBox);
+		},
+	setSubText: function(el){
+		if(!el)
+		{
+			var value = Glee.searchField.attr('value');
+			if(value !="")
+			{
+				if(value.indexOf('.com') != -1)
+				{
+					this.subText.html("Go to "+value);
+					this.subURL.html("http://"+value);
+				}
+				else
+				{
+					this.subText.html("Google "+value);
+					this.subURL.html("http://www.google.com/search?q="+value);
+				}
+			}
+			else
+			{
+				this.subText.html("No links selected");
+				this.subURL.html('');
+			}
+		}
+		else if(typeof(el)!= "undefined")
+		{
+			this.subText.html(el.text());
+			if(el.attr('title')!="" && el.attr('title')!=el.text())
+			{
+				this.subText.html(this.subText.html()+" -- "+el.attr('title'));
+			}
+			this.subURL.html(el.attr('href'));
+		}
+	},
+	scrollToLink: function(el){
+		var target = el;
+		if(target)
+		{
+			if(target.length)
+			{
+				var targetOffset = target.offset().top;
+				jQuery('html,body').animate({scrollTop:targetOffset},1000);
+				return false;
+			}
+		}
+	}
+}
+
+var LinkReaper = {
+	
+	searchTerm: "",
+	selectedLinks: [],
+	traversePosition: 0,
+	
+	reapLinks: function(term) {
+		if((LinkReaper.term != "") && (LinkReaper.searchTerm != term))
+		{
+			// If this term is a specialization of the last term
+			if((term.indexOf(LinkReaper.searchTerm) == 0) &&
+			(LinkReaper.searchTerm != ""))
+			{
+				jQuery(LinkReaper.selectedLinks).each(function(){
+					if(!LinkReaper.reapALink(jQuery(this), term))
+					{
+						LinkReaper.unreapLink(jQuery(this));
+						LinkReaper.selectedLinks = jQuery.grep(
+						LinkReaper.selectedLinks, 
+						function(val) {
+							return val != jQuery(this);
+						});
+					}
+				});
+			}
+			// Else search the whole page
+			else
+			{
+				newList = [];
+				jQuery('a').each(function(){
+					if(!LinkReaper.reapALink(jQuery(this), term))
+					{
+						LinkReaper.unreapLink(jQuery(this));
+						if(jQuery.inArray(jQuery(this), LinkReaper.selectedLinks) > -1)
+						{
+							LinkReaper.selectedLinks = jQuery.grep(
+								LinkReaper.selectedLinks, 
+								function(val) {
+									return val != jQuery(this);
+								});
+						}
+					}
+					else
+					{
+						newList.push(jQuery(this));
+					}
+				});
+				LinkReaper.selectedLinks = newList;
+			}
+			LinkReaper.searchTerm = term;
+			this.traversePosition = 0;
+		}
+	},
+	
+	reapALink: function(el, term) {
+		var index = el.text().toLowerCase().indexOf(term.toLowerCase());
+		if(index != -1) {
+			el.addClass('GleeReaped');
+			Glee.setSubText(el);
+			return true;
+		}
+		else {
+			return false;
+		}
+	},
+	
+	unreapLink: function(el) {
+		var isNotEqual = function(element){
+			if(element.attr('href') == el.attr('href'))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		};		
+		this.selectedLinks = this.selectedLinks.filter(isNotEqual);
+		el.removeClass('GleeReaped');
+	},
+	
+	unreapAllLinks: function() {
+		jQuery(LinkReaper.selectedLinks).each(function(){
+			LinkReaper.unreapLink(jQuery(this));
+		});
+		LinkReaper.selectedLinks.splice(0,LinkReaper.selectedLinks.length);
+		LinkReaper.searchTerm = "";
+		this.traversePosition = 0;
+	},
+	
+	getNextLink: function(){
+		if(this.selectedLinks.length == 0)
+		{
+			return null;
+		}
+		else if(this.traversePosition < this.selectedLinks.length - 1)
+		{
+
+			return this.selectedLinks[++this.traversePosition];
+		}
+		else
+		{
+			this.traversePosition = 0;
+			return this.selectedLinks[0];
+		}
+		
+	}
+}
