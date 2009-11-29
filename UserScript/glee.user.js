d2cb6c92a2db2432f2527a4dbf02d102d1098fc9
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-11-29 11:02:18 +0000

    added command to save to instapaper

--- UserScript/glee.user.js
+++ UserScript/glee.user.js
@@ -112,6 +112,10 @@ jQuery(document).ready(function(){
 					{
 						Glee.sendTweet();
 					}
+					else if(value == "!later") //command to save this page to read later (instapaper)
+					{
+						Glee.readLater();
+					}
 					else
 					{
 						LinkReaper.unreapAllLinks();
@@ -395,6 +399,11 @@ var Glee = {
 			var encodedURL = escape(location.href);
 			location.href =  "http://twitter.com/?status="+encodedURL;
 		}
+	},
+	readLater:function(){
+		//code via instapaper bookmarklet
+		Glee.setSubText("Saving to Instapaper...","msg");
+		location.href = "javascript:function%20iprl5(){var%20d=document,z=d.createElement('scr'+'ipt'),b=d.body;try{if(!b)throw(0);d.title='(Saving...)%20'+d.title;z.setAttribute('src','http://www.instapaper.com/j/3UK7xXDlnSJm?u='+encodeURIComponent(d.location.href)+'&t='+(new%20Date().getTime()));b.appendChild(z);}catch(e){alert('Please%20wait%20until%20the%20page%20has%20loaded.');}}iprl5();void(0)";
 	}
 
 }
