8175c45d7b558ac6fa1ac6309bcf2832eb8022d6
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-12-05 01:23:09 +0530

    updated description in userscript

--- UserScript/glee.user.js
+++ UserScript/glee.user.js
@@ -12,7 +12,7 @@
 // @name          gleeBox
 // @namespace     http://colloki.org/
 // @version		  0.5
-// @description   Keyboard goodness for your web
+// @description   gleeBox is an experimental project that takes a keyboard-centric approach to navigating the web. It provides the user with commands which she can run to perform actions that are traditionally performed using the mouse. It is thus mostly meant for keyboard and commandline lovers.
 // @include       *
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js
 // @require 	  http://json.org/json2.js
