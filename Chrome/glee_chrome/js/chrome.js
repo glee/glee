2099a047b60c55299b4f96d7b891969071c711be
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-12-14 01:01:27 +0530

    Moved chrome specific methods to a separate file 'chrome.js'

---
+++ Chrome/glee_chrome/js/chrome.js
@@ -0,0 +1,98 @@
+Glee.openNewTab = function(){
+	//sending request to background.html to create a new tab
+	chrome.extension.sendRequest({value:"createTab",url:Glee.URL},function(response){
+	});
+}
+
+Glee.isBookmark = function(text){
+	//send request to search the bookmark tree for the bookmark whose title matches text
+	chrome.extension.sendRequest({value:"getBookmarks",text:text},function(response){
+		if(response.bookmarks.length != 0) 
+		{
+			Glee.bookmarks = response.bookmarks;
+			Glee.bookmarks[Glee.bookmarks.length] = text;
+			Glee.currentResultIndex = 0;
+			Glee.setSubText(0,"bookmark");
+		}
+		else //google it
+		{
+			Glee.setSubText(text,"search");
+		}
+	});
+}
+
+Glee.getBookmarklet = function(text){
+	//sending request to get the first matched bookmarklet
+	chrome.extension.sendRequest({value:"getBookmarklet",text:text},function(response){
+		//if a bookmarklet is returned, run it
+		if(response.bookmarklet)
+			Glee.setSubText(response.bookmarklet,"bookmarklet");
+		else
+			Glee.setSubText("Command not found","msg");
+	});
+}
+
+Glee.sendRequest = function(url,method,callback){
+	//send request to background.html to send an XMLHTTPRequest
+	chrome.extension.sendRequest({value:"sendRequest",url:url,method:method},function(response){
+		callback(response.data);
+	});
+}
+
+Glee.getOptions = function(){
+	//sending request to get the gleeBox options
+	chrome.extension.sendRequest({value:"getOptions"},function(response){
+		
+		//gleeBox status i.e. enabled/disabled
+		Glee.status = response.status;
+		
+		//gleeBox position
+		if(response.position != null && response.position != 1) //by default, position is in middle anyways
+		{
+			if(response.position == 0) //top
+				Glee.position = "top";
+			else	//bottom
+				Glee.position = "bottom";
+		}
+		
+		//gleeBox Size
+		if(response.size != null && response.size != 2) //by default, size is large anyways
+		{
+			if(response.size == 0)
+				Glee.size = "small";
+			else if(response.size == 1)
+				Glee.size = 'medium';
+		}
+		
+		//Bookmark search
+		if(response.bookmark_search && response.bookmark_search == 1)
+			Glee.bookmarkSearchStatus = true; //enabled
+		else
+			Glee.bookmarkSearchStatus = false;
+			
+		//Scrolling animation
+		if(response.animation && response.animation == 1)
+			Glee.scrollingSpeed = 750; //enabled
+		else
+			Glee.scrollingSpeed = 0; //disabled
+		
+		//getting the restricted domains
+		if(response.domains)
+		{
+			for(var i=0;i<response.domains.length;i++)
+			{
+				Glee.domainsToBlock[Glee.domainsToBlock.length] = response.domains[i];
+			}
+		}
+		
+		Glee.checkDomain();
+		Glee.initOptions();
+	});
+}
+
+//adding a listener to respond to requests from background.html to update the status
+chrome.extension.onRequest.addListener(
+	function(request,sender,sendResponse){
+		Glee.status = request.status;
+		sendResponse({});
+});
--- Chrome/glee_chrome/js/glee.js
+++ Chrome/glee_chrome/js/glee.js
@@ -260,9 +260,7 @@ jQuery(document).ready(function(){
 					Glee.URL = Glee.makeURLAbsolute(Glee.URL, location.href);
 					if(e.shiftKey)
 					{
-						//sending request to background.html to create a new tab
-						chrome.extension.sendRequest({value:"createTab",url:Glee.URL},function(response){
-						});
+						Glee.openNewTab();
 						return false;
 					}
 					else
@@ -423,56 +421,6 @@ var Glee = {
 		jQuery(document.body).append(this.searchBox);
 		this.getOptions();
 		this.initOptions();
-	},
-	getOptions:function(){
-		//sending request to get the gleeBox options
-		chrome.extension.sendRequest({value:"getOptions"},function(response){
-			
-			//gleeBox status i.e. enabled/disabled
-			Glee.status = response.status;
-			
-			//gleeBox position
-			if(response.position != null && response.position != 1) //by default, position is in middle anyways
-			{
-				if(response.position == 0) //top
-					Glee.position = "top";
-				else	//bottom
-					Glee.position = "bottom";
-			}
-			
-			//gleeBox Size
-			if(response.size != null && response.size != 2) //by default, size is large anyways
-			{
-				if(response.size == 0)
-					Glee.size = "small";
-				else if(response.size == 1)
-					Glee.size = 'medium';
-			}
-			
-			//Bookmark search
-			if(response.bookmark_search && response.bookmark_search == 1)
-				Glee.bookmarkSearchStatus = true; //enabled
-			else
-				Glee.bookmarkSearchStatus = false;
-				
-			//Scrolling animation
-			if(response.animation && response.animation == 1)
-				Glee.scrollingSpeed = 750; //enabled
-			else
-				Glee.scrollingSpeed = 0; //disabled
-			
-			//getting the restricted domains
-			if(response.domains)
-			{
-				for(var i=0;i<response.domains.length;i++)
-				{
-					Glee.domainsToBlock[Glee.domainsToBlock.length] = response.domains[i];
-				}
-			}
-			
-			Glee.checkDomain();
-			Glee.initOptions();
-		});
 	},
 	initOptions:function(){
 		//setting gleeBox position
@@ -708,14 +656,10 @@ var Glee = {
 	},
 	simulateScroll: function(val){
 		jQuery('html,body').stop(true, true);
-		if(val == 1)
-		{
+		if(val == 1) //scroll up
 			window.scrollTo(window.pageXOffset,window.pageYOffset+200);
-		}
-		else if(val == 0)
-		{
+		else if(val == 0) //scroll down
 			window.scrollTo(window.pageXOffset,window.pageYOffset-200);
-		}
 		this.userPosBeforeGlee = window.pageYOffset;
 	},
 	simulateClick: function(el,target){
@@ -792,22 +736,6 @@ var Glee = {
 		var regex = new RegExp("(\\.(com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk|in))");
 		return url.match(regex);
 	},
-	isBookmark:function(text){
-		//send request to search the bookmark tree for the bookmark whose title matches text
-		chrome.extension.sendRequest({value:"getBookmarks",text:text},function(response){
-			if(response.bookmarks.length != 0) 
-			{
-				Glee.bookmarks = response.bookmarks;
-				Glee.bookmarks[Glee.bookmarks.length] = text;
-				Glee.currentResultIndex = 0;
-				Glee.setSubText(0,"bookmark");
-			}
-			else //google it
-			{
-				Glee.setSubText(text,"search");
-			}
-		});
-	},
 	checkDomain:function(){
 		for(var i=0; i<Glee.domainsToBlock.length; i++)
 		{
@@ -829,9 +757,7 @@ var Glee = {
 			for(var i=0;i<parents.length;i++)
 			{
 				if(jQuery(parents[i]).css("display") == "none")
-				{
 					return false;
-				}
 			}
 		}
 		return true;
@@ -845,13 +771,6 @@ var Glee = {
 		this.traversePosition = 0;
 		LinkReaper.searchTerm = "";
 	},
-	sendRequest: function(url,method,callback){
-		//send request to background.html to send an XMLHTTPRequest
-		chrome.extension.sendRequest({value:"sendRequest",url:url,method:method},function(response){	
-			callback(response.data);
-		});
-	},
-	
 	execCommand: function(command){
 		//call the method
 		//not sure if eval is the way to go here
@@ -859,16 +778,6 @@ var Glee = {
 		//setting the status
 		this.setSubText(command.statusText,"msg");
 		eval(method);
-	},
-	getBookmarklet: function(text){
-		//sending request to get the first matched bookmarklet
-		chrome.extension.sendRequest({value:"getBookmarklet",text:text},function(response){
-			//if a bookmarklet is returned, run it
-			if(response.bookmarklet)
-				Glee.setSubText(response.bookmarklet,"bookmarklet");
-			else
-				Glee.setSubText("Command not found","msg");
-		});
 	},
 	makeReadable: function(){
 		//code from the Readability bookmarklet (http://lab.arc90.com/experiments/readability/)
@@ -1070,10 +979,3 @@ var LinkReaper = {
 		el.addClass("GleeReaped");
 	}
 }
-
-//adding a listener to respond to requests from background.html to update the status 
-chrome.extension.onRequest.addListener(
-	function(request,sender,sendResponse){
-		Glee.status = request.status;
-		sendResponse({});
-});
