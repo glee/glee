2999e7395a0917c9aaac407ceb2625a9819dcb0f
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-12-27 23:20:12 +0530

    Made preferences pane functional

--- FF/defaults/preferences/gleebox.js
+++ FF/defaults/preferences/gleebox.js
@@ -1,8 +1,6 @@
-pref("extensions.gleebox.boolpref", false);
-pref("extensions.gleebox.intpref", 0);
-pref("extensions.gleebox.stringpref", "A string");
-
 pref("extensions.gleebox.size","medium");
 pref("extensions.gleebox.position","bottom");
 pref("extensions.gleebox.scroll_animation",false);
 pref("extensions.gleebox.theme","Greener");
+
+pref("browser.preferences.instantApply",true);
