5f1465ceca0552b80eb3593b782e0de503b1ae60
Ankit Ahuja <ahuja.ankit@gmail.com>
2009-12-29 17:28:37 +0530

    Got restricted domains option working in Prefs

--- FF/content/preferences.js
+++ FF/content/preferences.js
@@ -1,5 +1,6 @@
+var prefs = new gleebox_PrefManager();
+
 function initOptions(){
-	var prefs = new gleebox_PrefManager();
 	var disabled_urls_str = prefs.getValue('disabledurls',"");
 	if(disabled_urls_str != "")
 		var disabled_urls = disabled_urls_str.split(',');
@@ -7,22 +8,53 @@ function initOptions(){
 	for(var i=0; i<disabled_urls.length; i++)
 	{
 		var newItem = document.createElement("treeitem");
-                var newRow = document.createElement("treerow");
-                newItem.appendChild(newRow);
-                var newCell = document.createElement("treecell");
-                newCell.setAttribute("label",disabled_urls[i]);
-                newRow.appendChild(newCell);
+		var newRow = document.createElement("treerow");
+		newItem.appendChild(newRow);
+		var newCell = document.createElement("treecell"); 
+		newCell.setAttribute("label",disabled_urls[i]);
+		newRow.appendChild(newCell);
 		url_list.appendChild(newItem);
 	}
 }
 
-function saveDisabledURLList(){
+function getDisabledURLList(){
 	var disabled_urls = [];
-	var url_list = document.getElementById("disabled_url_list");
+	var url_list = document.getElementById("disabled_urls_list");
+        var tree = document.getElementById("disabled_urls_tree");
 	var listItems = url_list.childNodes;
-	for(var i=0; i<listItems.length; i++)
+        var row_being_edited = tree.editingRow;
+	for(var i=0; i<listItems.length; i++) 
 	{
-		disabled_urls[i] = listItems[i].getAttribute("label");
+		var row = listItems[i].childNodes[0];
+		var items = row.childNodes;
+		if(i == row_being_edited) 
+		{
+			disabled_urls[i] = tree.inputField.value;
+		} 
+		else
+			disabled_urls[i] = items[0].getAttribute("label");
 	}
 	return disabled_urls.join();
+}
+
+function addURL(){
+	var treeList = document.getElementById("disabled_urls_list");
+	var newItem = document.createElement("treeitem");
+	var newRow = document.createElement("treerow"); 
+	newItem.appendChild(newRow);
+	var newCell = document.createElement("treecell");
+	newRow.appendChild(newCell);
+	treeList.appendChild(newItem);
+	var tree = document.getElementById("disabled_urls_tree");
+	var treeCol = tree.columns[0];
+	tree.startEditing(treeList.childNodes.length-1,treeCol);
+}
+
+function removeURL(){
+	var tree = document.getElementById("disabled_urls_tree");
+	var itemIndex = tree.currentIndex;
+	var treeList = document.getElementById("disabled_urls_list");
+	var treeItem = treeList.childNodes[itemIndex];
+	treeList.removeChild(treeItem);
+	prefs.setValue('disabledurls', getDisabledURLList());
 }
