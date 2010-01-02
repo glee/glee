f67145ae0a4e03b4d66816c9483a45cd86a7ea7f
Ankit Ahuja <ahuja.ankit@gmail.com>
2010-01-02 19:15:00 +0530

    Changed FF ext. default prefs

--- FF/defaults/preferences/gleebox.js
+++ FF/defaults/preferences/gleebox.js
@@ -1,9 +1,9 @@
 pref("extensions.gleebox.status",true);
 pref("extensions.gleebox.size","medium");
-pref("extensions.gleebox.position","bottom");
+pref("extensions.gleebox.position","middle");
 pref("extensions.gleebox.scroll_animation",true);
 pref("extensions.gleebox.theme","Default");
 pref("extensions.gleebox.disabledurls","mail.google.com,wave.google.com");
-pref("extensions.gleebox.custom_scrapers","hn.ITEM.td[class=title]>a");
+pref("extensions.gleebox.custom_scrapers","g.ITEM.h3:not(ol.nobr>li>h3)");
 
 pref("browser.preferences.instantApply",true);
