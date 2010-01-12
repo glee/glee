8ba9973b7f640a7c49724f5bf31a4662364c42de
Ankit Ahuja <ahuja.ankit@gmail.com>
2010-01-12 16:57:43 +0530

    Added Prefs for ESP and search engine in FF extension

--- FF/content/preferences.js
+++ FF/content/preferences.js
@@ -42,6 +42,29 @@ var gleebox_Options = {
 				custom_scrapers_list.appendChild(newItem);
 			}
 		}
+		
+		//initializing ESP visions
+		var esp_visions_str = this.prefs.getValue("esp_visions","");
+		if(esp_visions_str != "")
+		{
+			var esp_visions_list = document.getElementById("esp_visions_list");
+			var esp_visions = esp_visions_str.split('.NEXT.');
+			var len = esp_visions.length;
+			for(i=0;i<len;i++) 
+			{
+				var pieces = esp_visions[i].split('.ITEM.');
+				var newItem = document.createElement("treeitem");
+				var newRow = document.createElement("treerow");
+				newItem.appendChild(newRow);
+				var s_name = document.createElement("treecell");
+				s_name.setAttribute("label",pieces[0]);
+				newRow.appendChild(s_name);
+				var s_sel = document.createElement("treecell");
+				s_sel.setAttribute("label",pieces[1]);
+				newRow.appendChild(s_sel);
+				esp_visions_list.appendChild(newItem);
+			}
+		}
 	},
 	getCustomScrapers: function(){
 		var prefString = "";
@@ -150,5 +173,80 @@ var gleebox_Options = {
 			treeList.removeChild(treeItem);
 			this.prefs.setValue('disabledurls', gleebox_Options.getDisabledURLList());
 		}
+	},
+	getESPVisions: function(){
+		var prefString = "";
+		var esp_visions_list = document.getElementById("esp_visions_list").childNodes;
+		var tree = document.getElementById("esp_visions_tree"); 
+		var row_being_edited = tree.editingRow;
+		var len = esp_visions_list.length;
+		for(var i=0; i<len; i++) 
+		{
+			var columns = esp_visions_list[i].childNodes[0].childNodes;
+			if(i == row_being_edited)
+			{
+				if(tree.editingColumn.index == 0 && columns[1]!=undefined)
+					var str=tree.inputField.value + ".ITEM." + columns[1].getAttribute("label");
+				else if(columns[0] != undefined)
+					var str=columns[0].getAttribute("label") + ".ITEM." + tree.inputField.value;
+			}  
+			else
+				var str = columns[0].getAttribute("label") + ".ITEM." + columns[1].getAttribute("label");
+		
+			if(prefString == "") 
+				prefString = str;
+			else
+				prefString += ".NEXT." + str;
+		}
+		return prefString;
+	},
+	addVision: function(){
+		var treeList = document.getElementById("esp_visions_list");
+		var newItem = document.createElement("treeitem"); 
+		var newRow = document.createElement("treerow");  
+		newItem.appendChild(newRow); 
+		var newCell = document.createElement("treecell");
+		newRow.appendChild(newCell); 
+		var secondCell = document.createElement("treecell");
+		newRow.appendChild(secondCell);
+		treeList.appendChild(newItem);
+		var tree = document.getElementById("esp_visions_tree");
+		var treeCol = tree.columns[0];
+		var newRowIndex = treeList.childNodes.length-1; 
+		tree.view.selection.select(newRowIndex);
+		tree.startEditing(newRowIndex,treeCol);
+		tree.inputField.addEventListener("keypress",gleebox_Options.goToNextESPField,true);
+	},
+	goToNextESPField: function(e){
+		if (! ((e.keyCode == KeyEvent.DOM_VK_TAB) || (e.keyCode == KeyEvent.DOM_VK_RETURN)))
+	    	return;
+		var tree = document.getElementById("esp_visions_tree");
+		var col = tree.editingColumn;
+		if(col.index == 1)
+			return;
+		col = col.getNext();
+		var row = tree.editingRow;
+		setTimeout(function() { tree.view.selection.select(row); tree.startEditing(row, col); }, 0);
+	},
+	removeVision: function(){
+		var tree = document.getElementById("esp_visions_tree");
+		var treeList = document.getElementById("esp_visions_list");
+		var itemIndex = tree.currentIndex;
+		if(itemIndex != -1 && tree.view.selection.isSelected(itemIndex))
+		{
+			var treeItem = treeList.childNodes[itemIndex];
+			treeList.removeChild(treeItem);
+			this.prefs.setValue('esp_visions', gleebox_Options.getESPVisions());
+		}
+	},
+	updateSearchEngine: function(value){
+		var textbox = document.getElementById("search_engine_textbox");
+		switch(value)
+		{
+			case "Google"	: textbox.value = "http://www.google.com/search?q="; break;
+			case "Bing"		: textbox.value = "http://www.bing.com/search?q="; break;
+			case "Yahoo"	: textbox.value = "http://search.yahoo.com/search?p="; break;
+		}
+		this.prefs.setValue('search_engine', textbox.value);
 	}
 };
--- FF/defaults/preferences/gleebox.js
+++ FF/defaults/preferences/gleebox.js
@@ -4,7 +4,10 @@ pref("extensions.gleebox.position","middle");
 pref("extensions.gleebox.scroll_animation",true);
 pref("extensions.gleebox.theme","Default");
 pref("extensions.gleebox.disabledurls","mail.google.com,wave.google.com");
-pref("extensions.gleebox.custom_scrapers","g.ITEM.h3:not(ol.nobr>li>h3)");
+pref("extensions.gleebox.custom_scrapers","");
 pref("extensions.gleebox.bookmark_search",false);
+pref("extensions.gleebox.search_engine","http://www.google.com/search?q=");
+pref("extensions.gleebox.esp_status",true);
+pref("extensions.gleebox.esp_visions","google.com/search.ITEM.h3:not(ol.nobr>li>h3).NEXT.bing.com/search.ITEM.div.sb_tlst");
 
 pref("browser.preferences.instantApply",true);
