b6c33137b8a92cb7c76f3f597bc00c18985e2905
Ankit Ahuja <ahuja.ankit@gmail.com>
2010-02-10 21:09:09 +0530

    gleeBox does not open when ctrl+g or cmd+g is pressed

--- Chrome/glee_chrome/js/glee.js
+++ Chrome/glee_chrome/js/glee.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function(){
 		if(Glee.status != 0)
 		{
 			//pressing 'g' if an input field is not focussed or alt+g(option+g on mac) anytime toggles the gleeBox
-			if(e.keyCode == 71 && ((target.nodeName.toLowerCase() != 'input' && target.nodeName.toLowerCase() != 'textarea' && target.nodeName.toLowerCase() != 'div') || e.altKey))
+			if(e.keyCode == 71 && !e.metaKey && !e.ctrlKey && ((target.nodeName.toLowerCase() != 'input' && target.nodeName.toLowerCase() != 'textarea' && target.nodeName.toLowerCase() != 'div') || e.altKey))
 			{
 				e.preventDefault();
 				Glee.userPosBeforeGlee = window.pageYOffset;
