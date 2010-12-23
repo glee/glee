4ba37bb8776bd6c58b526742f62bb2e0ec6bd2c6
Ankit Ahuja <ahuja.ankit@gmail.com>
2010-12-23 17:48:21 +0530

    added comments to Utils

--- Chrome/glee_chrome/js/utils.js
+++ Chrome/glee_chrome/js/utils.js
@@ -1,16 +1,29 @@
-/* Utility methods in Glee */
-
-Utils = {
+/**
+ *	Utility methods used in Glee
+ */
+var Utils = {
+	/**
+	 * 	Checks for a valid URL
+	 * 	@param {String} url String to be checked as a valid URL
+	 * 	@return {boolean} True if valid URL
+	 */
 	isURL: function(url) {
 		var regex = new RegExp("(\\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw))");
-		return url.match(regex);
+		return (url.match(regex) === null) ? false : true;
 	},
 	
+	/**
+	 * 	Converts a relative URL into absolute format
+	 * 	@param {String} link The url string
+	 * 	@param {String} host Host string
+	 * 	@return {String} The converted URL
+	 */
 	makeURLAbsolute: function(link, host) {
-		//check if its a bookmarklet meant to execute JS
-		if (link.indexOf("javascript:") == 0)
+		// if it is a bookmarklet, return as it is
+		if (link.indexOf("javascript:") === 0)
 			return link;
-		//code from http://github.com/stoyan/etc/blob/master/toAbs/absolute.html
+		
+		// from http://github.com/stoyan/etc/blob/master/toAbs/absolute.html
 		var lparts = link.split('/');
 		if (/http:|https:|ftp:/.test(lparts[0])) {
 			// already abs, return
@@ -58,18 +71,28 @@ Utils = {
 		return hparts.join('/') + '/' + newlinkparts.join('/');
 	},
 	
+	/**
+	 *	- Replaces < and > with their HTML character entities
+	 * 	- If the string is long, cut it off with ...
+	 * 	@param {String} text The string to operate on
+	 * 	@return {String} The filtered text
+	 */
 	filter: function(text) {
 		if (text && typeof(text) != "undefined")
 		{
-			//replace < with &lt; and > with &gt;
+			// replace < with &lt; and > with &gt;
 			text = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 			if (text.length > 75)
-				return text.substr(0, 73)+"...";
+				return text.substr(0, 73) + "...";
 			else
 				return text;
 		}
 	},
 	
+	/**
+	 *	Check if the current URL belongs to the list of disabled URLs. TODO: Does not belong here.
+	 *	@return {Integer} Returns 0 if yes. Else, 1.
+	 */
 	checkDomain: function() {
 	    var len = Glee.domainsToBlock.length;
 		for (var i = 0; i < len; i++)
@@ -80,33 +103,49 @@ Utils = {
 		return 1;
 	},
 	
+	/**
+	 * 	Checks if a DOM element and its parents are currently visible
+	 * 	@param {Element} el DOM element to check
+	 * 	@return {boolean} If the element is visible, returns true.
+	 */
 	isVisible: function(el) {
-		el = $(el);
-		if (el.css('display') == "none" || el.css('visibility') == "hidden")
+		var $el = $(el);
+		if ($el.css('display') === "none" || $el.css('visibility') === "hidden")
 			return false;
 		else
 		{
-			var parents = el.parents();
-			var len = parents.length;
+			var $parents = $el.parents();
+			var len = $parents.length;
+			
 			for (var i = 0; i < len; i++)
 			{
-				if ($(parents[i]).css("display") == "none")
+				if ($(parents[i]).css("display") === "none")
 					return false;
 			}
 		}
 		return true;
 	},
 	
+	/**
+	 *	Simulates a click on an element. TODO: el should be DOM element here
+	 * 	@param {jQuery} el jQuery object of DOM element to simulate click upon
+	 *	@param {boolean} Set to true if the link should be opened in a new tab
+	 * 	@return {Event} The Click event
+	 */
 	simulateClick: function(el, target) {
 		var evt = document.createEvent("MouseEvents");
-		//on Mac, pass target as e.metaKey
+		// on Mac, pass target as e.metaKey
 		if (navigator.platform.indexOf("Mac") != -1)
 			evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, target, 0, null);
-		else //otherwise, pass target as e.ctrlKey	
+		else // otherwise, pass target as e.ctrlKey	
 			evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, target, false, false, false, 0, null);
 		return el[0].dispatchEvent(evt);
 	},
 	
+	/**
+	 *	Start/stop scrolling
+	 * 	@param {Integer} val If set to 0, scrolling is stopped. Else, it determines whether to scroll upward/downward
+	 */
 	simulateScroll: function(val) {
 		if (val == 0) {
 			Glee.cache.jBody.stop(true);
@@ -118,6 +157,9 @@ Utils = {
 		}
 	},
 	
+	/**
+	 *	Set page into infinite scrolling mode
+	 */
 	infiniteScroll: function() {
 		if (Glee.scrollState < 0) {
 			loc = $(document).height();
@@ -132,14 +174,26 @@ Utils = {
 			duration);
 	},
 	
+	/**
+	 *	Selects all text in a textfield
+	 *	@param {Element} el Textfield whose text is to be selected
+	 * 	@return {boolean} If not a valid element or textfield is empty, returns false. Else, true.
+	 */
 	selectAllText: function(el) {
-        if (!el || !el.value || el.value == "")
+        if (!el || !el.value || el.value === "")
             return false;
         var len = el.value.length;
         el.setSelectionRange(0, len);
         return true;
     },
     
+	/**
+	 *	Make text editable in place. Replaces text with textfield for editing.
+	 *	@param {Element} el Element which contains the text
+	 *	@param {Function} callback Function to be called when user finishes editing
+	 *	@param {Object} options Options for edit in place field
+	 * 	@return {true}
+	 */
     makeEditable: function(el, callback, options) {
         el.bind('click keyup', {callback: callback}, function(e) {
             if (e.type == 'keyup' && e.keyCode != 13)
@@ -194,5 +248,6 @@ Utils = {
             input.bind('keyup', {input: input, el: el, callback: callback}, onClose);
             $(document).bind('mousedown',{input: input, el: el, callback: callback}, onClose);
         });
+		return true;
     }
-}
+};
