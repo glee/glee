a00c7ac5c92d692b6194022fa5567c734407263e
Chris Dary <umbrae@gmail.com>
2011-06-15 16:25:08 -0400

    Updating the Readability bookmarklet and adding Send to Kindle support.

--- UserScript/glee.user.js
+++ UserScript/glee.user.js
@@ -916,6 +916,7 @@ var Glee = {
 		this.setSubText(command.statusText,"msg");
 		eval(method);
 	},
+
 	makeReadable: function(){
 		//code from the Readability bookmarklets (http://www.readability.com/bookmarklet/)
 		location.href = "javascript:((function(){window.baseUrl='http://www.readability.com';window.readabilityToken='';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',baseUrl+'/bookmarklet/read.js');document.documentElement.appendChild(s);})());";
