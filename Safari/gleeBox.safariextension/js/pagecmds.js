3d4995de93b105cb15ceef0c7806f00206d03995
Ankit Ahuja <ahuja.ankit@gmail.com>
2011-06-25 21:34:30 +0530

    tweak to last commit: gleeBox is closed immediately instead of slight delay. updated version

--- Safari/gleeBox.safariextension/js/pagecmds.js
+++ Safari/gleeBox.safariextension/js/pagecmds.js
@@ -146,18 +146,14 @@ Glee.sharePage = function(newtab) {
 Glee.makeReadable = function() {
     // code from the Readability bookmarklet (http://lab.arc90.com/experiments/readability/)
     location.href = 'javascript:(%28function%28%29%7Bwindow.baseUrl%3D%27https%3A//www.readability.com%27%3Bwindow.readabilityToken%3D%27%27%3Bvar%20s%3Ddocument.createElement%28%27script%27%29%3Bs.setAttribute%28%27type%27%2C%27text/javascript%27%29%3Bs.setAttribute%28%27charset%27%2C%27UTF-8%27%29%3Bs.setAttribute%28%27src%27%2CbaseUrl%2B%27/bookmarklet/read.js%27%29%3Bdocument.documentElement.appendChild%28s%29%3B%7D%29%28%29);';
-    setTimeout(function() {
-        Glee.close();
-    }, 800);
+    Glee.close();
 };
 
 // kindle: Send the current page to your Kindle using Readability
 Glee.sendToKindle = function() {
     //code from the Readability bookmarklets (http://www.readability.com/bookmarklet/)
     location.href = 'javascript:(%28function%28%29%7Bwindow.baseUrl%3D%27https%3A//www.readability.com%27%3Bwindow.readabilityToken%3D%27%27%3Bvar%20s%3Ddocument.createElement%28%27script%27%29%3Bs.setAttribute%28%27type%27%2C%27text/javascript%27%29%3Bs.setAttribute%28%27charset%27%2C%27UTF-8%27%29%3Bs.setAttribute%28%27src%27%2CbaseUrl%2B%27/bookmarklet/send-to-kindle.js%27%29%3Bdocument.documentElement.appendChild%28s%29%3B%7D%29%28%29)';
-    setTimeout(function() {
-        Glee.close();
-    }, 800);
+    Glee.close();
 };
 
 // shorten: Shortens the current page's URL using bit.ly and displays the shortened URL in gleeBox
